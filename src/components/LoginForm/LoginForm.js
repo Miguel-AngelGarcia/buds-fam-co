eeb04359823e87708d93161163f35c73eb8b249d
Miguel-AngelGarcia <mgar574@wgu.edu>
2023-08-13 21:26:03 -0400

    project submit

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,14 @@
+export const LoginForm = () => {
+  return (
+    <div>
+      <div className="background">
+        <div className="modal-wrapper">
+          <div className="modal-container">
+            <div className="left-side"></div>
+            <div className="right-side"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
